44f952ec6d4a1be36182d6c18668e21089e2fd39
salzmarkt <ukangna@gmail.com>
2020-03-04 00:38:32 +0900

    Added views to create/edit blog post inside the site

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,13 +1,9 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
 urlpatterns = [
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from .models import Question
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 
@@ -11,16 +12,33 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myblog/post_list.html', {'posts': posts})
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'MYblog/post_detail.html', {'post': post})
+    return render(request, 'myblog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'myblog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'myblog/post_edit.html', {'form': form})
