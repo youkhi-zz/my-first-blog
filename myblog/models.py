a96eddab6bc32570f58ed161d9b8476de199f4fa
salzmarkt <ukangna@gmail.com>
2020-03-05 12:43:40 +0900

    last commit before fixing models.py

--- myblog/models.py
+++ myblog/models.py
@@ -20,16 +20,15 @@ class Choice(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #author = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-        
+
     def __str__(self):
         return self.title
