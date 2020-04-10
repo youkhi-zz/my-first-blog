15a3eb03f18761410444592fbca2e77b3abfe711
salzmarkt <ukangna@gmail.com>
2020-04-10 15:45:46 +0900

    echo chat completed

--- myblog/consumers.py
+++ myblog/consumers.py
@@ -4,15 +4,17 @@ import json
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        print("connected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         self.accept()
 
     def disconnect(self, close_code):
+        print("disconnected!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         pass
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        print(message)
         self.send(text_data=json.dumps({
             'message': message
         }))
--- mysite/settings.py
+++ mysite/settings.py
@@ -30,15 +30,15 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
-    #'channels',
+INSTALLED_APPS = [    
+    'channels',
     'myblog.apps.MyblogConfig',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
 ]
 '''
 CHANNEL_LAYERS = {
